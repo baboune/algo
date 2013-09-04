011e1ec2c73eda5bbf4328dfb41b451d67a2245f
lmcnise <nicolas.seyvet@ericsson.com>
2013-09-04 08:42:35 +0200

    add algorithm stuff

---
+++ algo/src/Percolation.java
@@ -0,0 +1,155 @@
+/**
+ *
+ * Copyright (c) Ericsson AB, 2013.
+ *
+ * All Rights Reserved. Reproduction in whole or in part is prohibited
+ * without the written consent of the copyright owner.
+ *
+ * ERICSSON MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. ERICSSON SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * User: lmcnise
+ * Date: 9/3/13
+ */
+
+import java.util.Arrays;
+
+/**
+ * comments.
+ */
+public class Percolation {
+    private static final int TOP = 0;
+    private static final int BOTTOM = 1;
+    private WeightedQuickUnionUF algo;
+    private int nMinus1;
+
+    private int[] sites;
+    private boolean[][] isOpens;
+    private int[][] theMap;
+
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        nMinus1 = N - 1;
+        // N * N plus top and bottom size
+        int gridSize = N * N;
+        algo = new WeightedQuickUnionUF(gridSize + 2);
+        isOpens = new boolean[N][N];
+        sites = new int[gridSize];
+        theMap = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int tot = i * N + j;
+                sites[tot] = tot + 2;
+                theMap[i][j] = tot + 2;
+            }
+        }
+        //System.out.println("algo count: " + algo.count());
+        //System.out.println("sites (total= " + sites.length + "): " + Arrays.toString(sites));
+        //System.out.println("TheMap ; ");
+        //for (int i = 0; i < N; i++) {
+        //    System.out.println("\t" + Arrays.toString(theMap[i]));
+        //}
+    }
+
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j) {
+        validateIJ(i, j);
+        if (!isOpen(i, j)) {
+            // open it
+            isOpens[i][j] = true;
+
+            // Check the unions -> connect with all adjacent sites
+            // not top/bottom/left/right
+            // i < N
+            // j < N
+            if (j > 0 && j < nMinus1) {
+                unionizeBorder(i, j);
+                if (isOpen(i, j + 1)) {
+                    //System.out.println("U union (" + theMap[i][j] + ", " + theMap[i][j + 1] + ")");
+                    algo.union(theMap[i][j], theMap[i][j + 1]);
+                }
+                if (isOpen(i, j - 1)) {
+                    //System.out.println("D union (" + theMap[i][j] + ", " + theMap[i][j - 1] + ")");
+                    algo.union(theMap[i][j], theMap[i][j - 1]);
+                }
+            } else if (j == nMinus1) {
+                //System.out.println("Connecting with top... (" + i + ", " + j + ")");
+                // Same except that we connect with TOP on j-1
+                unionizeBorder(i, j);
+                algo.union(theMap[i][j], TOP);
+                if (isOpen(i, j - 1)) {
+                    //System.out.println("D union (" + theMap[i][j] + ", " + theMap[i][j - 1] + ")");
+                    algo.union(theMap[i][j], theMap[i][j - 1]);
+                }
+            } else if (j == 0) {
+                // Same except that we connect with BOTTOM on j+1
+                unionizeBorder(i, j);
+                if (isOpen(i, j + 1)) {
+                    //System.out.println("U union (" + theMap[i][j] + ", " + theMap[i][j + 1] + ")");
+                    algo.union(theMap[i][j], theMap[i][j + 1]);
+                }
+                //System.out.println("Connecting with bottom... (" + i + ", " + j + ")");
+                algo.union(theMap[i][j], BOTTOM);
+            }
+        }
+    }
+
+    // is site (row i, column j) open?
+
+    public boolean isOpen(int i, int j) {
+        validateIJ(i, j);
+        return isOpens[i][j];
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        validateIJ(i, j);
+        return !isOpens[i][j];
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return algo.connected(TOP, BOTTOM);
+    }
+
+    private void unionizeBorder(int i, int j) {
+        if (i < (nMinus1) && isOpen(i + 1, j)) {
+            //System.out.println("R union (" + theMap[i][j] + ", " + theMap[i + 1][j] + ")");
+            algo.union(theMap[i][j], theMap[i + 1][j]);
+        }
+        if (i > 0 && isOpen(i - 1, j)) {
+            //System.out.println("L union (" + theMap[i][j] + ", " + theMap[i - 1][j] + ")");
+            algo.union(theMap[i][j], theMap[i - 1][j]);
+        }
+    }
+
+    private void validateIJ(final int i, int j) {
+        if (i < 0 || j < 0) {
+            throw new IllegalArgumentException("i and j must be positive integers");
+        }
+        if (i > nMinus1 || j > nMinus1) {
+            throw new IllegalArgumentException("i and j must be less than N(" + nMinus1 + ")");
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Percolation{");
+        sb.append(", sites=").append(Arrays.toString(sites));
+        sb.append(", isOpens=");
+        for (int j = 0; j <= nMinus1; j++) {
+            sb.append("\n\t [ ");
+            for(int i = 0; i <= nMinus1; i++)                                      {
+                sb.append(theMap[i][j]).append("(").append(isOpens[i][j]).append(") ");
+            }
+            sb.append("]");
+        }
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ algo/src/PercolationStats.java
@@ -0,0 +1,120 @@
+/**
+ *
+ * Copyright (c) Ericsson AB, 2013.
+ *
+ * All Rights Reserved. Reproduction in whole or in part is prohibited
+ * without the written consent of the copyright owner.
+ *
+ * ERICSSON MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. ERICSSON SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * User: lmcnise
+ * Date: 9/3/13
+ */
+
+
+import java.util.Random;
+
+/**
+ * comments.
+ */
+public class PercolationStats {
+    private double[] results;
+    private int nbOfExperiments;
+
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (T <= 0 || N <= 0) {
+            throw new IllegalArgumentException("T and N must be positive integers.");
+        }
+        double gridSize = N * N;
+        nbOfExperiments = T;
+        results = new double[T];
+
+        Random rnd = new Random();
+        int p, q;
+        for (int i = 0; i < results.length; i++) {
+            long counter = 0;
+            Percolation obj = new Percolation(N);
+            while (!obj.percolates()) {
+                p = rnd.nextInt(N);
+                q = rnd.nextInt(N);
+                if (!obj.isOpen(p, q)) {
+                    obj.open(p, q);
+                    counter++;
+                }
+                //System.out.println("status: " + obj);
+            }
+            System.out.println("(" + i + ") Percolated at: " + counter);
+            results[i] = counter / gridSize;
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        double r = 0;
+        for (double result : results) {
+            r += result;
+        }
+        return r / nbOfExperiments;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        double mean = mean();
+        return stddevWithMean(mean);
+    }
+
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo() {
+        double mean = mean();
+        double stddev = stddevWithMean(mean);
+        return mean - (1.96 * stddev / Math.sqrt(nbOfExperiments));
+    }
+
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi() {
+        double mean = mean();
+        double stddev = stddevWithMean(mean);
+        return mean + (1.96 * stddev / Math.sqrt(nbOfExperiments));
+    }
+
+    public double stddevWithMean(final double mean) {
+        if (nbOfExperiments == 1)
+            return Double.NaN;
+        double q = 0;
+        for (double result : results) {
+            q += (result - mean) * (result - mean);
+        }
+        return Math.sqrt(q / (nbOfExperiments - 1));
+    }
+
+    // test client, described below
+    //Your task is to write a computer program to estimate p*.
+    // p* the percolation threshold such as:
+    // - p < p* -> no percolation
+    // - p> p* -> percolates
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        System.out.println("Using N= " + N + ", T= " + T);
+
+        PercolationStats p = new PercolationStats(N, T);
+        double mean = p.mean();
+        double stddev = p.stddevWithMean(mean);
+        double hiConf = p.confidenceHi();
+        double loConf = p.confidenceLo();
+
+        System.out.println("mean\t\t\t\t\t\t= " + mean);
+        System.out.println("stddev\t\t\t\t\t\t= " + stddev);
+        System.out.println("95% confidence interval\t\t= " + loConf + ", " + hiConf);
+
+
+    }
+
+}
