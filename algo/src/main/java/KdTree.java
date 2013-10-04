fb0259f29ac2357e9098780be4d18af0e75efced
lmcnise <nicolas.seyvet@ericsson.com>
2013-10-04 09:15:38 +0200

    w5

---
+++ algo/src/main/java/KdTree.java
@@ -0,0 +1,60 @@
+/**
+ *
+ * Copyright (c) Baboune, 2013.
+ *
+ * All Rights Reserved. Reproduction in whole or in part is prohibited
+ * without the written consent of the copyright owner.
+ *
+ * ERICSSON MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. ERICSSON SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * User: Baboune
+ * Date: 10/4/13
+ */
+
+/**
+ * comments.
+ */
+public class KdTree {
+    // construct an empty set of points
+    public KdTree() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // number of points in the set
+    public int size() {
+        return -1;
+    }
+
+    // add the point p to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+    }
+
+    // does the set contain the point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // draw all of the points to standard draw
+    public void draw() {
+    }
+
+    // all points in the set that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // a nearest neighbor in the set to p; null if set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+}
